b860bbe9c247112a79aa08c2e87a1d5d0a63b0bd
Molly Whitnack <mwhitnac@ucsd.edu>
2015-02-20 10:41:11 +0000

    sfds

--- static/routes/Tabbed.js
+++ static/routes/Tabbed.js
@@ -1,68 +1,82 @@
 var data = require("../common/json/data.json");
 var course = require("../common/json/course.json");
 
+var count = [];
+var t1 = 0;
 exports.view = function(req, res)
 { 
-	// console.log(course);
 	res.render('Tabbed', data);
-	//console.log(data);
-	//var tCourse = req.params.course;
-	//var listing[];
 	for(var i = 0; i < data.owner.length; i++)
 	{
-	console.log("--------Tab------: " + data.owner[i].course);
+		var tabmatch = null;
 	  for(var j = 0; j<data.sessions.length; j++)
 	  {	//add matching session to owner's couse
 		if( data.sessions[j].course == data.owner[i].course)
 		{
 
-		 console.log("-------sessionMatch---------");
+			 var tabmatch = 
+		 	 {	
+			 "course": data.sessions[j].course,
+			 "name" : data.sessions[j].name,
+			 "location": data.sessions[j].location,
+			 "timeleft": data.sessions[j].timeleft,
+			 "professor":data.sessions[j].professor,
+			 "sessionid": data.sessions[j].sessionid
+    		 };
+    	//console.log(tabmatch);
 
-		 var tabmatch = 
-		 {	
-		 "course": data.sessions[j].course,
-		 "name" : data.sessions[j].name,
-		 "location": data.sessions[j].location,
-		 "timeleft": data.sessions[j].timeleft,
-		 "professor":data.sessions[j].professor,
-		 "sessionid": data.sessions[j].sessionid
-    	  };
-    	console.log(tabmatch);
-
-    	if(i ==0 )
+    	if(i == 0 )
     	{
-    
-    	data['course1'].push(tabmatch);
-    	console.log("pushed to course1");
+    	var n1 = data.course1.length;
+      	console.log("n1 :" +  n1 + " at t1:  " + t1);
+    	//var course1{}
+    	data.course1.push(tabmatch);
+    	console.log();
+    	//console.log(tabmatch);
+    	//console.log("pushed to course1");
+    	//console.log("n: " + n1);
 		}
-    	if(i == 1)
+    	if(i == 1 )
     	{
     	data['course2'].push(tabmatch);
-		console.log("pushed to course2");
+    	console.log();
+    	//console.log(tabmatch);
+		//console.log("pushed to course2");
 		}
 		if(i ==2 )
     	{
     	data['course3'].push(tabmatch);
-    	console.log("pushed to course1");
+    	    	console.log();
+    	//console.log(tabmatch);
+    	//console.log("pushed to course3");
 		}
     	if(i == 3)
     	{
     	data['course4'].push(tabmatch);
-		console.log("pushed to course2");
+    	    	console.log();
+    	//console.log(tabmatch);
+		//console.log("pushed to course4");
 		}
 		if(i ==4 )
     	{
     	data['course5'].push(tabmatch);
-    	console.log("pushed to course1");
+    	    	console.log();
+    	//console.log(tabmatch);
+    	//console.log("pushed to course5");
 		}
     	else
     	{
-		console.log("Not pushed");
+
 		}
+		
+		tabmatch = {};
 	 	
       }
-  }
-	console.log("------ end ------");
+
+  	}
+  	t1 = n1;
+  	console.log("t1: " + t1);
+  
 	console.log();
 	}
 	
