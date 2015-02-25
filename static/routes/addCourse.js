f65b5f1ad4160a6d21336afaf479c742f6c6b8aa
Zhihui <zhxia@ucsd.edu>
2015-02-25 13:04:25 -0800

    aaa

--- static/routes/addCourse.js
+++ static/routes/addCourse.js
@@ -7,37 +7,27 @@ exports.view = function(req, res) {
 	//console.log(newFriends);
 	//res.render('add',data);
 	// jQuery.each(course,function(){
-		// if()
+	// if()
 	// });
     var course = req.query.course;
     var professor = req.query.professor;
     if(course != null && course != "") {
-    var newCourse = {
-			"course": course,
-			"professor": professor,	
-    };
-
-    if (data.owner.length < 5)
+		var newCourse = {
+				"course": course,
+				"professor": professor,	
+		};
+	
+    if (courses.owner.length < 5)
     {
     courses["owner"].push(newCourse);
 	console.log(newCourse);
     }
     else
     {
-        console.log("Can't add 6 courses")};
+        console.log("Can't add 6 courses");
     }
+	}
 
   res.render('addCourse',courses);
-}
+};
 
-function validateForm() {
-	console.log("validating form...");   
-    if (document.forms["addcourse"]["course"].value == null || document.forms["addcourse"]["course"].value == "") {
-        alert("You must enter a course ID");
-        return false;
-    }
-    if (document.forms["addcourse"]["professor"].value == null || document.forms["addcourse"]["professor"].value == "") {
-        alert("Please enter professor's name");
-        return false;
-    }
-}
